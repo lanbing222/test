405d236a7c2d0b19eca813c3cb1cef0a9e5790e7
lanbing222 <lanbing222zhu@126.com>
2017-04-22 21:43:11 +0800

    add replace

--- replace.py
+++ replace.py
@@ -1,8 +1,3 @@
-# import os
-# import sys
-# import tab
-# with open("ip.txt","r") as f:
-#     print f.readline()
 import fileinput
 for line in fileinput.input("ip.txt",backup='bak',inplace=0):
     line=line.replace("192.168.0.1","myip")
